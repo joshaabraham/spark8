1b22cdc465f30b9020771ed18e5606d2d8ad2f78
rems <duplan.remi@gmail.com>
2022-05-29 15:40:46 -0400

    changed stuff about comment block and mainConteneur references

--- src/app/layout/components/main-conteneur/main-conteneur.module.ts
+++ src/app/layout/components/main-conteneur/main-conteneur.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MainConteneurComponent } from './main-conteneur.component';
 
+import { CommentBlockModule } from '../comment-block/coment-block.module';
+
 @NgModule({
     declarations: [
       MainConteneurComponent
     ],
     imports     : [
-
+        CommentBlockModule
     ],
     exports     : [
        MainConteneurComponent
